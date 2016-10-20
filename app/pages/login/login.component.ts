b61863000034881dd3ec5e2b728be0132d45e244
Alberto Fernández <albertof.rubio@colaboradoresunidadeditorial.es>
2016-10-20 15:26:48 +0200

    Second commit

--- app/pages/login/login.component.ts
+++ app/pages/login/login.component.ts
@@ -5,6 +5,8 @@ import { UserService } from "../../shared/user/user.service";
 import { Page } from "ui/page";
 import { Color } from "color";
 import { View } from "ui/core/view";
+import { setHintColor } from "../../utils/hint-util";
+import { TextField } from "ui/text-field";
 
 @Component({
   selector: "my-app",
@@ -17,12 +19,13 @@ export class LoginComponent implements OnInit {
   user: User;
   isLoggingIn = true;
   @ViewChild("container") container: ElementRef;
+  @ViewChild("email") email: ElementRef;
+  @ViewChild("password") password: ElementRef;
 
   constructor(
     private router: Router,
     private userService: UserService,
-    private page: Page)
-  {
+    private page: Page) {
     this.user = new User();
   }
   ngOnInit() {
@@ -31,6 +34,7 @@ export class LoginComponent implements OnInit {
   }
   toggleDisplay() {
   this.isLoggingIn = !this.isLoggingIn;
+  this.setTextFieldColors();
   let container = <View>this.container.nativeElement;
   container.animate({
     backgroundColor: this.isLoggingIn ? new Color("white") : new Color("#301217"),
@@ -38,6 +42,10 @@ export class LoginComponent implements OnInit {
   });
   }
   submit() {
+      if (!this.user.isValidEmail()) {
+  alert("Enter a valid email address.");
+  return;
+}
     if (this.isLoggingIn) {
       this.login();
     } else {
@@ -61,4 +69,16 @@ export class LoginComponent implements OnInit {
       () => alert("Unfortunately we were unable to create your account.")
     );
 }
+  setTextFieldColors() {
+    let emailTextField = <TextField>this.email.nativeElement;
+    let passwordTextField = <TextField>this.password.nativeElement;
+
+    let mainTextColor = new Color(this.isLoggingIn ? "black" : "#C4AFB4");
+    emailTextField.color = mainTextColor;
+    passwordTextField.color = mainTextColor;
+
+    let hintColor = new Color(this.isLoggingIn ? "#ACA6A7" : "#C4AFB4");
+    setHintColor({ view: emailTextField, color: hintColor });
+    setHintColor({ view: passwordTextField, color: hintColor });
+    }
 }
